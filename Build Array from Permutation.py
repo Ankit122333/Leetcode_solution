8dc41a164dc1ee2ba3baa141f54aaadaa3e71454
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2022-03-31 13:01:14 +0530

    Delete Build Array from Permutation.py

--- Build Array from Permutation.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def buildArray(self, nums: List[int]) -> List[int]:
-        ans=[]
-        for i in range(0,len(nums)):
-            x=nums[nums[i]]
-            ans.append(x)
-        return ans
