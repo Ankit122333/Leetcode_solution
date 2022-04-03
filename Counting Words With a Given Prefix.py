944bf6ccbc71262286d205b7f7b0dec570986d4a
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2022-04-03 23:07:58 +0530

    Delete Counting Words With a Given Prefix.py

--- Counting Words With a Given Prefix.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def prefixCount(self, words: List[str], pref: str) -> int:
-        count=0
-        for i in range(len(words)):
-            if(words[i].startswith(pref)):
-                count+=1
-        return count
