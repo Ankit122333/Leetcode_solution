1a735e33b0d62413e7141bf21d30a2cc323807e4
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2022-06-22 23:59:45 +0530

    Delete Find First Palindromic String in the Array.py

--- Find First Palindromic String in the Array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def firstPalindrome(self, words: List[str]) -> str:
-        for w in words:
-            if w == w[:: -1]:
-                return w
-        return ''
-        
