45a94ea1e2007bee7de8f7a2608ed25da9876fae
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2022-06-22 23:58:52 +0530

    Delete Find All K-Distant Indices in an Array.py

--- Find All K-Distant Indices in an Array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
-        ans = set()
-        for i, num in enumerate(nums):
-            if num == key:
-                ans.update(range(max(0, i-k), min(i+k+1, len(nums))))
-        return sorted(list(ans))
