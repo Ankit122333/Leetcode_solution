ad43f8921a2d808f0be46f9df55050e1446343ef
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2022-06-23 00:00:40 +0530

    Delete Intersection of Two Arrays II.py

--- Intersection of Two Arrays II.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums = []
-        for i in nums1:
-            if i in nums2:
-                nums.append(i)
-                nums2.remove(i)
-                    
-        return nums
