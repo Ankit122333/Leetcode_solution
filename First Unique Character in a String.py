30cf0e81da8967a1c09cb7108eec83a2cbd9ac8d
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2022-06-23 00:00:17 +0530

    Delete First Unique Character in a String.py

--- First Unique Character in a String.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        c=Counter(s)
-        for i in range(len(s)):
-            if c[s[i]]==1:
-                return i
-        return -1
