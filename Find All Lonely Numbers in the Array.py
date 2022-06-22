8248c2c915d125e3bf970ea36c6165c9e5f5bf02
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2022-06-22 23:59:05 +0530

    Delete Find All Lonely Numbers in the Array.py

--- Find All Lonely Numbers in the Array.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def findLonely(self, nums: List[int]) -> List[int]:
-        C = collections.Counter(nums)
-        res = []
-        for k, v in C.items():
-            if v == 1 and C[k-1] == 0 and C[k+1] == 0:
-                res.append(k)
-        return res
