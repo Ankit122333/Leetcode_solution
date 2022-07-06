7769a0546be32e8651f489de4205bf519454d750
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2022-07-06 17:49:22 +0530

    Delete Keep Multiplying Found Values by Two.py

--- Keep Multiplying Found Values by Two.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findFinalValue(self, nums: List[int], original: int):
-        '''for i in range(len(nums)):
-            if(nums[i]==original):
-                i=0
-                original *= 2'''
-        while original in nums:
-            original*=2
-        return original
