030eb51226007f66fe0b390afec5b90f965daaa7
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2023-01-31 20:22:21 +0530

    Delete Sorting the Sentence.py

--- Sorting the Sentence.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def sortSentence(self, s: str) -> str:
-        x = s.split()
-        res = {}
-        for i in x :
-            res[i[-1]] = i[:-1]
-        return ' '.join([res[j] for j in sorted(res)])
-        
