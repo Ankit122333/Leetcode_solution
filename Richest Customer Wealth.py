99f823ab39e4dfbb398a9717c8e5304a8b049615
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2023-01-31 20:20:55 +0530

    Delete Richest Customer Wealth.py

--- Richest Customer Wealth.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def maximumWealth(self, accounts: List[List[int]]) -> int:
-        maxi,sum1=0,0
-        for i in range(len(accounts)):
-            sum1=sum(accounts[i])
-            maxi=max(sum1,maxi)
-        return maxi
