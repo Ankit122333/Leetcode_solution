6322fde4c1ff895b45f141608985412576a34a91
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2023-01-31 20:21:47 +0530

    Delete Shuffle the Array.py

--- Shuffle the Array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def shuffle(self, nums: List[int], n: int) -> List[int]:
-        ans=[]
-        r = len(nums) // 2
-        for l in range(len(nums) // 2):
-            ans.append(nums[l])
-            ans.append(nums[r])
-            r += 1
-        return ans
