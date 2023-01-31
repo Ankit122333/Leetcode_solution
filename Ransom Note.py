96bf3b4522f4ff3cb4b56146e15eb2620715b83c
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2023-01-31 20:19:51 +0530

    Delete Ransom Note.py

--- Ransom Note.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str):
-        for i in set(ransomNote):
-                if ransomNote.count(i)> magazine.count(i):
-                    return False
-        return True
