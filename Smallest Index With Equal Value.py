26fed5de81712b3cacb0c0457175b8ec5a4e9d18
ANKIT KUMAR <74482735+Ankit122333@users.noreply.github.com>
2023-01-31 20:22:00 +0530

    Delete Smallest Index With Equal Value.py

--- Smallest Index With Equal Value.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def smallestEqual(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
-            if (i%10==nums[i]):
-                return i
-        else: 
-            return -1
